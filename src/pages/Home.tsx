fb056b2f1a5cbdde14bc3b8bb6b5c10c193e033c
codewithcojo <breigns60@gmail.com>
2026-08-04 19:38:36 +0000

    added service section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import Hero from "@/components/Hero";
 import About from "@/components/About";
+import ServicesSection from "@/components/ServicesSection";
 import Projects from "@/components/Projects";
 import Contact from "@/components/Contact";
 
@@ -8,6 +9,7 @@ const Home = () => {
     <main>
       <Hero />
       <About />
+      <ServicesSection />
       <Projects />
       <Contact />
     </main>
